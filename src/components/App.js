228f56ab4dc429aa6ab91d9ec915ce583d5bf6d5
Sergey Timofeev <drumliker@yandex.ru>
2023-08-13 13:43:16 +0300

    Подключили MockApi

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import '../styles/app.scss';
 import Header from './Header';
 import Categories from './Categories';
 import Sort from './Sort';
 import PizzaBlock from './PizzaBlock';
-import pizzas from '../assets/pizzas.json'
+// import pizzas from '../assets/pizzas.json'
 
 function App() {
+  const [pizzas, setPizzas] = useState([]);
+  useEffect(() => {
+    // Проверки
+    fetch('https://64d8ae0a5f9bf5b879ce72a8.mockapi.io/items')
+      .then(resp => resp.json())
+      .then(data => setPizzas(data));
+  }, [])
+
   return (
     <div className="wrapper">
       <Header />
