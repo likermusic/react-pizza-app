0a5d79f6f27cffc85a8a7e68af97da8eb9d2d7d3
Sergey Timofeev <drumliker@yandex.ru>
2023-10-15 12:05:43 +0300

    делаем корзину

--- src/components/PizzaBlock.js
+++ src/components/PizzaBlock.js
@@ -1,87 +1,88 @@
-import React, { useMemo, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import { addItem } from '../store/slices/cartSlice';
+import React, { useMemo, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addItem } from "../store/slices/cartSlice";
 
-function PizzaBlock({ id, imageUrl,
+function PizzaBlock({
+  id,
+  imageUrl,
   title,
   types,
   sizes,
   price,
   category,
-  rating }) {
-
-  const cartItems = useSelector(state => state.cart.items);
+  rating,
+}) {
+  const item = { id, imageUrl, title, price };
+  const cartItems = useSelector((state) => state.cart.items);
   const dispatch = useDispatch();
 
   const [activeSize, setActiveSize] = useState(0);
   const [activeType, setActiveType] = useState(0);
 
-  //useMemo не работает
-  // const ind = useMemo(() => {
-  //   return cartItems.findIndex(item => item.id == id);
-  // }, [id])
+  const ind = useMemo(() => {
+    const ind = cartItems.findIndex((item) => item.id == id);
+    return ind;
+  }, [id, cartItems]);
 
-  const ind = cartItems.findIndex(item => item.id == id);
   let qty = 0;
-
   if (ind != -1) {
     qty = cartItems[ind].qty;
   }
 
-
   return (
-    <div className="pizza-block">
-      <img
-        className="pizza-block__image"
-        src={imageUrl}
-        alt="Pizza"
-      />
-      <h4 className="pizza-block__title">{title}</h4>
-      <div className="pizza-block__selector">
+    <div className='pizza-block'>
+      <img className='pizza-block__image' src={imageUrl} alt='Pizza' />
+      <h4 className='pizza-block__title'>{title}</h4>
+      <div className='pizza-block__selector'>
         <ul>
-          {
-            types.map(type => (
-              (types.length > 1) ?
-                <li onClick={() => setActiveType(type)} key={type} className={type == activeType ? 'active' : ''}>
-                  {type == 0 ? 'тонкое' : 'традиционное'}
-                </li>
-                :
-                <li key={type} className="active">
-                  {type == 0 ? 'тонкое' : 'традиционное'}
-                </li>
-            ))
-          }
-
+          {types.map((type) =>
+            types.length > 1 ? (
+              <li
+                onClick={() => setActiveType(type)}
+                key={type}
+                className={type == activeType ? "active" : ""}>
+                {type == 0 ? "тонкое" : "традиционное"}
+              </li>
+            ) : (
+              <li key={type} className='active'>
+                {type == 0 ? "тонкое" : "традиционное"}
+              </li>
+            )
+          )}
         </ul>
         <ul>
-          {
-            sizes.map((size, ind) => (
-              <li onClick={() => setActiveSize(ind)} key={size} className={ind == activeSize ? "active" : ""}>{size} см.</li>
-            ))
-          }
+          {sizes.map((size, ind) => (
+            <li
+              onClick={() => setActiveSize(ind)}
+              key={size}
+              className={ind == activeSize ? "active" : ""}>
+              {size} см.
+            </li>
+          ))}
         </ul>
       </div>
-      <div className="pizza-block__bottom">
-        <div className="pizza-block__price">от {price} ₽</div>
-        <div className="button button--outline button--add">
+      <div className='pizza-block__bottom'>
+        <div className='pizza-block__price'>от {price} ₽</div>
+        <div
+          className='button button--outline button--add'
+          onClick={() => dispatch(addItem(item))}>
           <svg
-            width="12"
-            height="12"
-            viewBox="0 0 12 12"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
+            width='12'
+            height='12'
+            viewBox='0 0 12 12'
+            fill='none'
+            xmlns='http://www.w3.org/2000/svg'>
             <path
-              d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
-              fill="white"
+              d='M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z'
+              fill='white'
             />
           </svg>
-          <span onClick={() => dispatch(addItem(id))}>Добавить</span>
+          <span>Добавить</span>
           <i>{qty}</i>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default PizzaBlock
+export default PizzaBlock;
--- src/store/slices/cartSlice.js
+++ src/store/slices/cartSlice.js
@@ -1,40 +1,34 @@
 import { createSlice } from "@reduxjs/toolkit";
-
+import pizzasSlice from "./pizzasSlice";
 
 //Получили pizzas из pizzasSlice
 const initialState = {
-  items: [{ id: 1, qty: 3 }], // [{id:1,c:3},{id:1,c:3},{id:1,c:3}]
+  items: [],  [{id:1,imageUrl, title, price,qty:3}]
   total: 0,
-  count: 0
-}
-
+  count: 0,
+};
 
 const cartSlice = createSlice({
-  name: 'cart',
+  name: "cart",
   initialState,
   reducers: {
     addItem(state, action) {
-      const id = action.payload;
-      const ind = state.items.findIndex(item => item.id == id);
+      const id = action.payload.id;
+      const ind = state.items.findIndex((item) => item.id == id);
       if (ind == -1) {
-        const item = { id, qty: 1 };
-        state.items.push(item);
+        state.items.push({ ...action.payload, qty: 1 });
       } else {
         state.items[ind].qty += 1;
       }
 
       //как прочитать state здесь
-      //как получить state из другого слайса
-      state.count = state.items.reduce((count, item) => count + item.qty, 0)
-      // state.total = 
+      // console.log(JSON.stringify(state.items));
+      // state.count = state.items.reduce((count, item) => count + item.qty, 0);
+      // state.total =
     },
-    deleteItem(state, action) {
-
-    }
-  }
-})
+    deleteItem(state, action) {},
+  },
+});
 
 export const { addItem, deleteItem } = cartSlice.actions;
 export default cartSlice.reducer;
-
-
