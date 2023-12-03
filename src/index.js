669e633eb03e7d8133141de37e6e8c2a2b38088c
Sergey Timofeev <drumliker@yandex.ru>
2023-12-03 10:52:25 +0300

    Удалили мусор 2

--- src/index.js
+++ src/index.js
@@ -4,12 +4,10 @@ import "./index.css";
 import "./styles/app.scss";
 import App from "./components/App";
 import { BrowserRouter } from "react-router-dom";
-import App1 from "./components/App1";
 // import AppRouter from './routes/AppRouter';
 
 import { Provider } from "react-redux";
 import { store } from "./store";
-import App2 from "./components/App2";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
